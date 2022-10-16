8c475bfc3eef63156a6c0eccfc2ef6e9a692f1e7
yuliu-debond <79855548+yuliu-debond@users.noreply.github.com>
2022-10-16 11:22:02 +0200

    Update test.sol

--- test.sol
+++ test.sol
@@ -5,9 +5,13 @@ pragma solidity ^0.8.0;
 interface IERC6595 {
     // STRUCTURE 
     /**
-     * @dev Values structure of the Metadata, cited from ERC-3475
+     * @dev metadata and Values structure of the Metadata, cited from ERC-3475
      */
-
+    struct Metadata {
+        string title;
+        string _type;
+        string description;
+    }
     struct Values { 
         string stringValue;
         uint uintValue;
@@ -17,26 +21,38 @@ interface IERC6595 {
 
     /**
      * @dev structure that defines the parameters for specific issuance of bonds and amount which are to be transferred/issued/given allowance, etc.
-     * @notice this structure is used for the verification process, it chontains the metadataID, logic, expectation
-     * @metadataID is the ID of the metadatas need to be verified
+     * @notice this structure is used for the verification process, it chontains the SBFID and logic
      * @logic given here MUST be either ("and", "or")
      */
-
     struct Verification {
         uint256 SBFID;
         string logic;
-        }
-    
-    function verifiy(address verifying, Verification memory proofNeeded) external view returns (bool);
+    }
+
+    struct Requirement {
+        uint256 metadataID;
+        string logic;
+        string expectation;
+    }
+
+    function verifiy(address verifying, Verification[] memory proofNeeded) external view returns (bool);
+    function standardRequirement(uint256 SBFID) external view returns (Requirement[] memory);
+    function changeStandardRequirement(uint256 SBFID, Requirement[] memory) external view returns (bool);
+    function certify(address certifying, uint256 SBFID) external returns (bool);
+    function revoke(address certifying, uint256 SBFID) external returns (bool);
+
+    event standardChanged(uint256 SBFID, Requirement[]);   
+    event certified(address certifying, uint256 SBFID);
+    event revoked(address certifying, uint256 SBFID);
+
 }
 
 abstract contract ERC6595 is IERC6595 {
     address private _authenticator;
-    Verification private _KYCRequirement;
-    uint public test;
+    Verification[] private _KYCRequirement;
 
-    constructor(address authenticatorAddress, Verification memory KYCStandard) {
-        _KYCRequirement = KYCStandard;
+    constructor(address authenticatorAddress, Verification[] memory KYCStandards) {
+        _KYCRequirement = KYCStandards;
         _authenticator = authenticatorAddress;
     }
 
@@ -46,8 +62,10 @@ abstract contract ERC6595 is IERC6595 {
     }
 }
 
-abstract contract Token is ERC6595 {
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+abstract contract Token is ERC6595 {
+    uint public test;
     function mint(address to, uint256 amount) public KYCApproved(to){
         _mint(to, amount);
     }
